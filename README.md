bd8f5ab21666128c86faa93dbbea2acee289e49a
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2016-10-26 20:14:40 +0200

    Licenza

--- README.md
+++ README.md
@@ -5,9 +5,13 @@ Connette Wordpress e SPID, in qualche modo. Per ora non connette nulla, a dire i
 **Work in progress**
 
 ## TODO
-- [ ] Scegliere una licenza consona (GPLv3+, MIT, o Apache 2.0?)
+- [X] Scegliere una licenza consona (GPLv3+, MIT, o Apache 2.0?)
 - [ ] Capire come funziona SPID
 - [ ] Capire come si scrive un plugin Wordpress
 - [ ] Utilizzare la libreria [SimpleSpidphp](https://github.com/dev4pa/simplespidphp)
 - [ ] Trarre ispirazione da [SPID-Drupal](https://github.com/dev4pa/spid-drupal)
 - [ ] Scrivere il codice.
+
+## Licenza
+
+GPLv3 o successive.
--- spid-wordpress.php
+++ spid-wordpress.php
@@ -1,11 +1,28 @@
 <?php
+/*
+    SPID-Wordpress - Plugin che connette Wordpress e SPID
+    Copyright (C) 2016 Ludovico Pavesi
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 /*
-Plugin Name: Spid Wordpress
+Plugin Name: SPID-Wordpress
 Plugin URI: http://
 Description: Fa cose con SPID
 Version: 0.1
 Author:
 Author URI: http://
-License:
+License: GPLv3+
 */
