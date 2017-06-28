bc469c5536ccfba0f2bfe7e0765b47452de85cb4
Valerio Bozzolan <valerio.bozzolan@gmail.com>
2017-06-28 03:18:38 +0200

    back to newlines

--- spid-wordpress/public/class-spid-wordpress-public.php
+++ spid-wordpress/public/class-spid-wordpress-public.php
@@ -38,23 +38,24 @@
  * @todo       Use this class for something. Or delete it.
  */
 class Spid_Wordpress_Public {
-    protected $plugin_name;
+	protected $plugin_name;
 
-    protected $version;
-    /**
+	protected $version;
+
+	/**
 	 * Initialize the class and set its properties.
 	 *
-	 * @since    1.0.0
+	 * @since 1.0.0
 	 */
 	public function __construct() {
-        $this->plugin_name  = 'spid-login';
-        $this->version = '1.0.0';
+		$this->plugin_name = 'spid-login';
+		$this->version = '1.0.0';
 	}
 
 	/**
 	 * Register the stylesheets for the public-facing side of the site.
 	 *
-	 * @since    1.0.0
+	 * @since 1.0.0
 	 */
 	public function enqueue_styles() {
 
@@ -72,13 +73,12 @@ class Spid_Wordpress_Public {
 
 		//wp_enqueue_style( Spid_Wordpress::PLUGIN_NAME, plugin_dir_url( __FILE__ ) . 'css/spid-wordpress-public.css', array(), $this->version, 'all' );
 
-
 	}
 
 	/**
 	 * Register the JavaScript for the public-facing side of the site.
 	 *
-	 * @since    1.0.0
+	 * @since 1.0.0
 	 */
 	public function enqueue_scripts() {
 		/**
@@ -94,55 +94,54 @@ class Spid_Wordpress_Public {
 		 */
 
 		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/spid-sp-access-button.min.js', array( 'jquery' ), $this->version, true );
-        wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/spid-sp-access-button.min.css', array(), $this->version, 'all' );
+		wp_enqueue_style(  $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/spid-sp-access-button.min.css', array(), $this->version, 'all' );
 
 	}
 
+	/**
+	 * Print the button on login page
+	 * @since 1.0.0
+	 */
+	public function print_button() {
 
-    /**
-     * Print the button on login page
-     * @since   1.0.0
-     */
-    public function print_button() {
+		//echo apply_filters('spid/login_button', '<div><h1>Login</h1></div>');
+		//echo "<div><h1>Login</h1></div>";
+		include_once('partials' . DIRECTORY_SEPARATOR . 'spid-wordpress-button.php');
 
-        //echo apply_filters('spid/login_button', '<div><h1>Login</h1></div>');
-        //echo "<div><h1>Login</h1></div>";
-        include_once('partials/spid-wordpress-button.php');
+	}
 
-    }
+	/**
+	 * The name of the plugin used to uniquely identify it within the context of
+	 * WordPress and to define internationalization functionality.
+	 *
+	 * @since 1.0.0
+	 * @return string The name of the plugin.
+	 */
+	public function get_plugin_name() {
+		return $this->plugin_name;
+	}
 
-    /**
-     * The name of the plugin used to uniquely identify it within the context of
-     * WordPress and to define internationalization functionality.
-     *
-     * @since     1.0.0
-     * @return    string    The name of the plugin.
-     */
-    public function get_plugin_name() {
-        return $this->plugin_name;
-    }
+	public function get_img($name) {
+		//echo $this->path;
+		//echo $this->thisFile;
+		echo plugins_url('img/' . $name, __FILE__);
+	}
 
-    public function get_img($name) {
-        //echo $this->path;
-        //echo $this->thisFile;
-        echo plugins_url('img/' . $name,__FILE__);
+	public function add_spid_scripts(){
+		?>
 
-    }
+		<script>
+		jQuery(document).ready( function () {
+			var rootList = jQuery('#spid-idp-list-small-root-get');
+			var idpList = rootList.children('.spid-idp-button-link');
+			var lnkList = rootList.children('.spid-idp-support-link');
+			while (idpList.length) {
+				rootList.append( idpList.splice(Math.floor(Math.random() * idpList.length), 1)[0] );
+			}
+			rootList.append(lnkList);
+		} );
+		</script>
 
-    public function add_spid_scripts(){
-        ?>
-        <script>
-            jQuery(document).ready(function(){
-                var rootList = jQuery("#spid-idp-list-small-root-get");
-                var idpList = rootList.children(".spid-idp-button-link");
-                var lnkList = rootList.children(".spid-idp-support-link");
-                while (idpList.length) {
-                    rootList.append(idpList.splice(Math.floor(Math.random() * idpList.length), 1)[0]);
-                }
-                rootList.append(lnkList);
-            });
-            </script>
-<?php
-    }
+		<?php
+	}
 }
-?>
